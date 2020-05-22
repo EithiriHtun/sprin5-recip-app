fb0b426cfd0b89a9154e62985048ec31dcbf61a7
eithiri <eithiritun1997@gmail.com>
2020-05-22 10:34:48 +0630

    Adding New Recipe Form #198

--- src/main/java/guru/springframework/spring5recipapp/controller/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipapp/controller/RecipeController.java
@@ -1,11 +1,11 @@
 package guru.springframework.spring5recipapp.controller;
 
+import guru.springframework.spring5recipapp.commands.RecipeCommand;
 import guru.springframework.spring5recipapp.services.RecipeService;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -20,5 +20,19 @@ public class RecipeController {
     public String GetRecipebyId(@PathVariable String id, Model model) {
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String SaveOrUpdate(@ModelAttribute RecipeCommand command, Model model){
+      RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/"+ savedCommand.getId();
     }
 }
