f0365c0b3a64efb1025295c961f542522556c51d
eithiri <eithiritun1997@gmail.com>
2020-05-27 09:03:24 +0630

    Adding Error Page #227

--- src/test/java/guru/springframework/spring5recipapp/controller/RecipeControllerTest.java
+++ src/test/java/guru/springframework/spring5recipapp/controller/RecipeControllerTest.java
@@ -52,13 +52,13 @@ class RecipeControllerTest {
     }
 
     @Test
-    public void testGetRecipeNotFound() throws Exception{
+    public void testGetRecipeNotFound() throws Exception {
 
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
-        mockMvc.perform(get("/recipe/show"))
-                .andExpect(status().isNotFound());
+
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
     @Test
